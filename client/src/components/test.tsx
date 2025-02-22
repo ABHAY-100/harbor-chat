15997b18d94899f3f556e02e1bd96ba96e013d8d
sreyas <sreyas1045@gmail.com>
2025-02-22 15:37:35 +0530

    setting up socket connection

--- client/src/components/test.tsx
+++ client/src/components/test.tsx
@@ -7,21 +7,31 @@ const socket: Socket = io("http://localhost:5000");
 export default function Chat() {
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState<string[]>([]);
-
+    const [from, setFrom] = useState<string>();
+    const [messagesUser2, setMessagesUser2] = useState<string>();
+    const myUserId = "user1";
+    const myPublicKey = "User1PublicKey";    // used by others to encrypt messages to you
+    const myPrivateKey = "User1PrivateKey";  // used to decrypt received messages
+    
+    socket.emit("register" , { userId: myUserId, publicKey: myPublicKey })
     useEffect(() => {
-        socket.on("message", (msg: string) => {
-            setMessages((prev) => [...prev, msg]);
+        socket.on("private message", (from : string , message: string) => {
+            setMessages((prev) => [...prev, message]);
+            console.log(`recieveed from ${from}`)
+            setFrom(from)
+            setMessagesUser2(message)
         });
 
         return () => {
-            socket.off("message");
+            socket.off("private message");
         };
     }, []);
 
     const sendMessage = () => {
         if (message.trim()) {
-            socket.emit("message", message);
+            socket.emit("private message", {to : "Abhay" , message : message});
             setMessage("");
+        console.log(from)
         }
     };
 
@@ -33,12 +43,16 @@ export default function Chat() {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
             />
-            <button onClick={sendMessage}>Send</button>
+            <button onClick={sendMessage} >Send</button>
+        
             <ul>
                 {messages.map((msg, index) => (
                     <li key={index}>{msg}</li>
                 ))}
+                
+
             </ul>
+            <p>{from} {messagesUser2}</p>
         </div>
     );
 }
